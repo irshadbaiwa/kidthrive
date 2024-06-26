dc4bf377236b943cd13f036cc5e3b0f22bf7fdc7
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-06-26 17:24:41 +0100

    add child screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,14 @@
-import { View } from 'react-native';
-import CustomText from '@/components/CustomText';
-import ScreenLayout from '@/components/ScreenLayout';
-import { Button } from '@rneui/themed';
-import { Link } from 'expo-router';
+import { View } from "react-native";
+import CustomText from "@/components/CustomText";
+import ScreenLayout from "@/components/ScreenLayout";
+import { Button } from "@rneui/themed";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <ScreenLayout title="Home">
       <CustomText>Edit app/index.tsx to edit this screeen</CustomText>
-      <Link href="/add-child" asChild>
+      <Link href="/child" asChild>
         <Button>Add child</Button>
       </Link>
     </ScreenLayout>
